5b3a9e9c243c17fdcccb835a35a3575dea376e0b
mattral <minhtetmyet2630@gmail.com>
2024-08-05 20:30:05 +0700

    fixing

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,6 +1,6 @@
 import { SignUp } from "@clerk/nextjs";
 
-export default function () {
+export default function SiginUpPage() {
   return (
     <main className="flex h-screen w-full items-center justify-center">
       <SignUp />
